fc6e8c81349561b0b119e5a850699f9f43e4844e
PyryM <pkmatikainen@gmail.com>
2014-05-08 16:10:07 -0400

    save_frame.py takes better arguments

--- src/utils/save_frame.py
+++ src/utils/save_frame.py
@@ -3,21 +3,59 @@ import numpy as np
 import cv2
 import pygray
 import pygrayutils
+import argparse
 
-# todo: convert this to argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename",
+                    help="Filename to save image as.", default="frame.png")
+parser.add_argument("-i", "--index",
+                    help="Camera index (not consistent!)", type=int, default=0)
+parser.add_argument("-s", "--serialnumber",
+                    help="Camera serial number", type=int, default=0)
+parser.add_argument("-c", "--color", help="Whether to use color",
+                    type=bool, default=False)
+parser.add_argument("--list",
+                    help="List cameras (overrides all other options)",
+                    action="store_true")
+args = parser.parse_args()
+
+
+def print_cameras(camlist):
+    for guid in camlist:
+        cam = pygray.Camera(guid)
+        print(repr(cam.getinfo()))
+
+
+def get_cam_by_serial(serialnum, camlist):
+    for guid in camlist:
+        cam = pygray.Camera(guid)
+        caminfo = cam.getinfo()
+        if caminfo.serial_number == serialnum:
+            return cam
+    return None
+
 cams = pygray.listcams()
-tarcam = int(sys.argv[1])
-destfn = sys.argv[2]
-usecolor = (int(sys.argv[3]) > 0)
+cam = None
 
-cam = pygray.Camera(cams[tarcam])
-caminfo = cam.getInfo()
-if usecolor:
-    cam.setColorMode(True)
+if args.list:
+    print_cameras(cams)
+    return
+
+if args.serialnumber > 0:
+    cam = get_cam_by_serial(args.serialnumber)
+    if not cam:
+        print("No camera found with serial number %d" % args.serialnumber)
+        print("Run with --list argument to get camera information.")
+        return
+else:
+    cam = pygray.Camera(cams[args.index])
+
+if args.color:
+    cam.setcolormode(True)
 cam.start()
-camdata = cam.getFrameStr()
+camdata = cam.getframe()
 cam.stop()
 
 imdata = pygrayutils.framestr_to_array(camdata)
-      
-cv2.imwrite(destfn, imdata)
+
+cv2.imwrite(args.filename, imdata)
